531d108a119ffe05a2ac3605919c990e434eaf3c
Thaismoises <129102979+Thaismoises@users.noreply.github.com>
2023-03-27 19:57:27 +0100

    Opção  de saída
    Incluso a opção  de saída para o usuário

--- Exercicio4.c
+++ Exercicio4.c
@@ -129,6 +129,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+        printf("\t4 - Sair do sistema\n\n");
 		printf("opcao: "); // Fim do menu
 
 		scanf("%d", &opcao); // armazenando a escolha do ususario
@@ -149,6 +150,12 @@ int main()
 			deletar();
 			break;
 
+        case 4:
+                printf("Obrigado por utilizar o sistema!\n");
+                return 0;
+                break;
+                
+                
 		default:
 			printf("Essa opcao nao esta disponivel!\n");
 			system("pause");
